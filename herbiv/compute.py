e4b27c14ce644845b62fea1aac480df2dcf7d811
Wesady <Wesady@qq.com>
2024-04-06 17:32:26 +0800

    更新score函数，改为新的计算方法

--- herbiv/compute.py
+++ herbiv/compute.py
@@ -5,60 +5,89 @@ import random
 from tqdm import tqdm
 
 
-def score(tcm,
-          tcm_chem_links,
-          chem,
-          chem_protein_links,
-          formula=None,
-          formula_tcm_links=None):
+def score(tcm: pd.DataFrame,
+          tcm_chem_links: pd.DataFrame,
+          chem: pd.DataFrame,
+          chem_protein_links: pd.DataFrame,
+          formula: Union[pd.DataFrame, None] = None,
+          formula_tcm_links: Union[pd.DataFrame, None] = None,
+          weights: Union[dict, None] = None) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
-    计算化合物和中药的Importance Score
-    :param formula_tcm_links:
-    :param formula:
-    :param tcm: pd.DataFrame类型，中药信息
-    :param tcm_chem_links: pd.DataFrame类型，中药-化合物（中药成分）连接信息
-    :param chem: pd.DataFrame类型，化合物（中药成分）信息
-    :param chem_protein_links: pd.DataFrame类型，化合物（中药成分）-蛋白质（靶点）连接信息
-    :return: chem: pd.DataFrame类型，化合物的Importance Score的计算结果
-    :return: tcm: pd.DataFrame类型，中药的Importance Score的计算结果
+        计算复方、中药和化合物的HerbiV Score。
+
+        Args:
+            tcm: 要计算HerbiV Score的中药的信息，格式与get.get_tcm的返回值相同。
+            tcm_chem_links: tcm和chem中的中药-成分（化合物）连接信息。
+            chem: 要计算HerbiV Score的化合物的信息，格式与get.get_chemicals的返回值相同。
+            chem_protein_links: chem和拟分析靶点（蛋白）的化合物（中药成分）-蛋白质（靶点）连接信息。
+            formula: 要计算HerbiV Score的复方信息，格式与get.get_formulas的返回值相同。默认为None。
+            formula_tcm_links: formula和tcm中的复方-中药连接信息。默认为None。
+            weights: 各靶点（蛋白）的权重，各权重的和应为1。默认为None。
+
+        Returns:
+            包含HerbiV Score的复方、中药和成分（化合物）的信息。
+
+            tcm_and_score: 中药信息及HerbiV Score。
+            chem_and_score: 成分（化合物）信息及HerbiV Score。
+            formula_and_score: 复方信息及HerbiV Score。
     """
 
-    tcm_c = tcm.copy()
-    chem_c = chem.copy()
+    tcm_and_score = tcm.copy()
+    chem_and_score = chem.copy()
+    formula_and_score = formula.copy()
 
-    # 计算化合物的Importance Score
-    chem_c['Importance Score'] = chem_c.loc[:, 'HVCID'].apply(
-        lambda x: 1 - (
-                1 - np.array([*chem_protein_links.loc[chem_protein_links['HVCID'] == x]['Combined_score']])).prod())
+    proteins_id = chem_protein_links['Ensembl_ID'].unique()
 
-    # 计算中药的Importance Score
-    tcm_c['Importance Score'] = tcm_c.loc[:, 'HVMID'].apply(
-        lambda x: 1 - (1 - np.array([*chem_c.loc[
-            chem_c['HVCID'].isin(tcm_chem_links.loc[tcm_chem_links['HVMID'] == x]['HVCID'])][
-            'Importance Score']])).prod())
+    # 计算与各蛋白对应的HerbiV Score
+    for protein in proteins_id:
+        # 计算每一个化合物的HerbiV Score
+        chem_and_score[protein + ' HerbiV Score'] = chem.loc[:, 'HVCID'].apply(lambda x: 1 - (1 - np.array(
+            [*chem_protein_links.loc[(chem_protein_links['HVCID'] == x) &
+                                     (chem_protein_links['Ensembl_ID'] == protein)]['Combined_score']])
+                                                                                              ).prod()
+                                                                               )
 
-    # 若传入了复方相关信息，则需计算复方的Importance Score
+        # 计算中药的HerbiV Score
+        tcm_and_score[protein + ' HerbiV Score'] = tcm.loc[:, 'HVMID'].apply(lambda x: 1 - (1 - np.array(
+            [*chem_and_score.loc[chem_and_score['HVCID'].isin(
+                tcm_chem_links.loc[tcm_chem_links['HVMID'] == x]['HVCID'])][protein + ' HerbiV Score']])
+                                                                                            ).prod()
+                                                                             )
+
+        # 若传入了复方相关信息，则需计算复方的HerbiV Score
+        if formula is not None:
+            formula_and_score[protein + ' HerbiV Score'] = formula.loc[:, 'HVPID'].apply(lambda x: 1 - (1 - np.array(
+                [*tcm_and_score.loc[tcm_and_score['HVMID'].isin(
+                    formula_tcm_links.loc[formula_tcm_links['HVPID'] == x]['HVMID'])][protein + ' HerbiV Score']])
+                                                                                                        ).prod()
+                                                                                         )
+        else:
+            formula_and_score = None
+
+    # TODO: 验证各权重的和是否为1
+    # 计算与每一个蛋白对应的HerbiV Score
+    if weights is None:
+        weights = {col: 1 for col in [protein + ' HerbiV Score' for protein in proteins_id]}
+
+    # TODO: 将所有Importance Score替换为HerbiV Score
     if formula is not None:
-        formula_c = formula.copy()
-        formula_c['Importance Score'] = formula_c.loc[:, 'HVPID'].apply(
-            lambda x: 1 - (1 - np.array([*tcm_c.loc[
-                tcm_c['HVMID'].isin(formula_tcm_links.loc[formula_tcm_links['HVPID'] == x]['HVMID'])][
-                'Importance Score']])).prod())
+        formula_and_score['Importance Score'] = (formula_and_score[list(weights.keys())] * pd.Series(weights)).mean(axis=1)
         # 根据Importance Score降序排序
-        formula_c = formula_c.sort_values(by='Importance Score', ascending=False)
+        formula_and_score = formula_and_score.sort_values(by='Importance Score', ascending=False)
         # 重新设置索引
-        formula_c.index = range(formula_c.shape[0])
-    else:
-        formula_c = None
+        formula_and_score.index = range(formula_and_score.shape[0])
+    tcm_and_score['Importance Score'] = (tcm_and_score[list(weights.keys())] * pd.Series(weights)).mean(axis=1)
+    chem_and_score['Importance Score'] = (chem_and_score[list(weights.keys())] * pd.Series(weights)).mean(axis=1)
 
     # 根据Importance Score降序排序
-    tcm_c = tcm_c.sort_values(by='Importance Score', ascending=False)
+    tcm_and_score = tcm_and_score.sort_values(by='Importance Score', ascending=False)
+    chem_and_score = chem_and_score.sort_values(by='Importance Score', ascending=False)
 
     # 重新设置索引
-    tcm_c.index = range(tcm_c.shape[0])
-    chem_c.index = range(chem_c.shape[0])
+    tcm_and_score.index = range(tcm_and_score.shape[0])
+    chem_and_score.index = range(chem_and_score.shape[0])
 
-    return tcm_c, chem_c, formula_c
+    return tcm_and_score, chem_and_score, formula_and_score
 
 
 def component(items_and_score, random_state=None, num=1000, c=10):
